f600b7436c4eb96df79c14f95ef3f6323abaa02c
Kami <kamillynascimento63176@gmail.com>
2026-05-09 20:48:22 -0300

    Update Desafio 3.sql

--- Oficina mecanica/Desafio 3.sql
+++ Oficina mecanica/Desafio 3.sql
@@ -1,5 +1,5 @@
-INSERT INTO `SELECT 
-    SUM(os.valor_total) AS faturamento_total
+INSERT INTO 
+SELECT SUM(os.valor_total) AS faturamento_total
 FROM ordens_servico os
 JOIN veiculos v ON os.id_veiculo = v.id_veiculo
 JOIN clientes c ON v.id_cliente = c.id_cliente
