28c80e6ee65df72ec41e7bea1a849136786a3644
Kami <kamillynascimento63176@gmail.com>
2026-05-09 20:46:32 -0300

    Create Desafio 3.sql

---
+++ Oficina mecanica/Desafio 3.sql
@@ -0,0 +1,7 @@
+INSERT INTO `SELECT 
+    SUM(os.valor_total) AS faturamento_total
+FROM ordens_servico os
+JOIN veiculos v ON os.id_veiculo = v.id_veiculo
+JOIN clientes c ON v.id_cliente = c.id_cliente
+WHERE c.cidade = 'São Paulo'` (faturamento_total) VALUES
+	 (1400.0);
